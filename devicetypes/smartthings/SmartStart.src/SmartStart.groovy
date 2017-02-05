19f5946ead0582d7122817a91f4bcad9bbbb5ed2
Rob Leonard <rleonard55@gmail.com>
2017-02-05 11:19:46 -0600

    Update SmartStart.groovy
    Cleaned up whitespace

--- devicetypes/smartthings/SmartStart.src/SmartStart.groovy
+++ devicetypes/smartthings/SmartStart.src/SmartStart.groovy
@@ -3,7 +3,7 @@
 // TODO: Needs state check implemented sense start/stop calls the same action on 
 metadata 
 {
-    definition(
+	definition(
         name: "Smart Start",
         namespace: "SmartStart",
         author: "rleonard55",	
@@ -12,16 +12,16 @@ metadata
         iconX2Url:"http://www.fgcaraudio.com/viper_smart_start.png",
         singleInstance: true)
         {
-            capability "timedSession"
-            capability "lock"
+		capability "timedSession"
+		capability "lock"
         }
 	
 	preferences 
 	{
-        input("Username", "string", title:"Insteon Hub Username", description: "Please enter your SmartStart Username", defaultValue: "user" , required: true, displayDuringSetup: true)
-        input("Password", "password", title:"Insteon Hub Password", description: "Please enter your SmartStart Password", defaultValue: "password" , required: true, displayDuringSetup: true)
+		input("Username", "string", title:"Insteon Hub Username", description: "Please enter your SmartStart Username", defaultValue: "user" , required: true, displayDuringSetup: true)
+		input("Password", "password", title:"Insteon Hub Password", description: "Please enter your SmartStart Password", defaultValue: "password" , required: true, displayDuringSetup: true)
 		//input("VehicleName", "string", title:"SmartStart Vehicle Name", description: "Please enter your SmartStart Vehicle Name", defaultValue: "My Car" , required: true, displayDuringSetup: true)
-    }
+    	}
     
 	simulator { }
 
@@ -32,7 +32,7 @@ metadata
 			state "on", label: 'Start', action: "timedSession.start", icon: "st.Kids.kid10", backgroundColor: "#ffffff", nextState: "off"
 			state "off", label: 'Stop', action: "timedSession.stop", icon: "st.Kids.kid10", backgroundColor: "#79b821", nextState: "on"
 		}
-        standardTile("button", "device.lock", width: 2, height: 2, canChangeIcon: true) 
+        	standardTile("button", "device.lock", width: 2, height: 2, canChangeIcon: true) 
 		{
 			state "on", label: 'Lock', action: "lock.lock", icon: "st.Kids.kid10", backgroundColor: "#ffffff", nextState: "off"
 			state "off", label: 'Unlock', action: "lock.unlock", icon: "st.Kids.kid10", backgroundColor: "#79b821", nextState: "on"
@@ -51,75 +51,75 @@ private getVechicleName() { getDevicePreferenceByName(device, "VehicleName") }
 	
 def lock() 
 {
-    log.info "Locking"
-    send("arm")
+	log.info "Locking"
+	send("arm")
 }
 
 def unlock() 
 {
-     log.info "Unlocking"
-     send("disarm")
+	log.info "Unlocking"
+	send("disarm")
 }
 
 def start()
 {
 	log.info "Starting"
-    send("remote")
+	send("remote")
 }
 
 def stop()
 {
 	log.info "Stoping"
-    send("remote")
+	send("remote")
 }
 
 private send (Action)
 {	
-    def SessionKey = ""
-    def VechicleId = ""
+	def SessionKey = ""
+	def VechicleId = ""
     
 	log.info "Logging in"
 	SessionKey = login()
 	log.debug "Success! => Logged in successfully!"
 
 	log.debug "Finding vehicle "+ getVechicleName() 
-    VechicleId = GetVehicleID(SessionKey)
-    log.debug "Success! => Vechicle Id found successfully!"
+	VechicleId = GetVehicleID(SessionKey)
+	log.debug "Success! => Vechicle Id found successfully!"
     
-    log.debug "Sending "+ Action+ " to "+ VechicleId + " with session key " + SessionKey
-    SendAction(VechicleId, Action, SessionKey)
+	log.debug "Sending "+ Action+ " to "+ VechicleId + " with session key " + SessionKey
+	SendAction(VechicleId, Action, SessionKey)
 }
 
 private SendAction(VechicleId, Action, SessionId)
 {
 	def url = getSendCommandUrl(VechicleId, Action, SessionId)
-    log.debug "Send Action Url is "+ url
+	log.debug "Send Action Url is "+ url
     
-    httpGet(url) { resp -> log.debug resp.data }
+	httpGet(url) { resp -> log.debug resp.data }
 }
 
 private GetVehicleID(SessionId)
 {
 	log.debug "Building Get Vehicle URL"
-    def VehicleId = ""
-    def url = getVehicleIdUrl(SessionId)
-    log.debug "Get Vehicle URL: "+ url
+	def VehicleId = ""
+	def url = getVehicleIdUrl(SessionId)
+	log.debug "Get Vehicle URL: "+ url
     
-    // TODO: Loop through devices and find device by VehicleName
-    httpGet(url) { resp -> VehicleId = resp.data.Return.Results.Devices[0].DeviceId }
+	// TODO: Loop through devices and find device by VehicleName
+	httpGet(url) { resp -> VehicleId = resp.data.Return.Results.Devices[0].DeviceId }
     
-    log.debug "Returning Vehicle Id: " + VehicleId
-    return VehicleId
+	log.debug "Returning Vehicle Id: " + VehicleId
+	return VehicleId
 }
 
 private login()
 {
 	log.debug "Building login URL"
-    def SessionKey = ""
-    def url = getLoginUrl(getUsername(),getPassword())
-    log.debug "the url is: "+ url
+	def SessionKey = ""
+	def url = getLoginUrl(getUsername(),getPassword())
+	log.debug "the url is: "+ url
     
-    httpGet(url) { resp -> SessionKey = resp.data.Return.Results.SessionID }
-    log.debug "Received Session Key: " + SessionKey
-    return SessionKey
+	httpGet(url) { resp -> SessionKey = resp.data.Return.Results.SessionID }
+	log.debug "Received Session Key: " + SessionKey
+	return SessionKey
 }
